840dcc1e7e4dbd641f8c3c8a3f9a36bcae0f0189
alistair <133965877+BuildHare@users.noreply.github.com>
2023-06-20 08:50:22 +1000

    Add heirachy of results and outcome text

--- game.js
+++ game.js
@@ -1,25 +1,55 @@
 function getComputerChoice () {
     let compChoice = Math.floor (Math.random() * 3);
-    alert(compChoice)
     if (compChoice === 0) {
-        let decision = ("BEE BOO BEE BOO GAMEBOT HAS SELECTED Rock")
-        alert (decision)
+        let decision = ("Rock")
+        return (decision)
     }
     else if (compChoice === 1) {
-        let decision = ("BEE BOO BEE BOO GAMEBOT HAS SELECTED Paper") 
-        alert (decision)
+        let decision = ("Paper") 
+        return (decision)
     }
     else if (compChoice === 2) {
-        let decision = ("BEE BOO BEE BOO GAMEBOT HAS SELECTED Scissors")
-        alert (decision)
+        let decision = ("Scissors")
+        return (decision)
     }
 
 } 
-//Computer choice generator is random as of break
+
 function rockPaperScissors () {
-    let playerChoice = (prompt("Rock, Paper or Scissors?"))
-    let computerChoice = getComputerChoice()
-    if (playerChoice === "Rock" && computerChoice === 0) {
+    const playerChoice = (prompt("Rock, Paper or Scissors?"))
+    const computerChoice = getComputerChoice()
+    alert (computerChoice)
+    if (playerChoice === "Rock" && computerChoice === "Rock") {
         alert("Shoot! You both picked rock! Draw!")
     }
+    else if (playerChoice ==="Rock" && computerChoice === "Scissors") {
+        alert("Rock crushes the enemy! VICTORY!")
+    }
+    else if (playerChoice ==="Rock" && computerChoice === "Paper") {
+        alert("It's paper! We are defeated!")
+    }
+    else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+        alert("Rock! Noooooooo")
+    }
+    else if (playerChoice === "Scissors" && computerChoice === "Scissors") {
+        alert("Another Scissors! TIE!")
+    }
+    else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        alert("Scissors slices Paper! VICTORY!")
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        alert("Die Rock! VICTORY!")
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        alert("It's Scissors! Noooooo!")
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Paper") {
+        alert("Finally, a worthy opponent! Our battle shall be legendary!")
+    }
 }
+
+
+//Computer choice generator is random as of break
+// computerChoice cannot read result from getComputerChoice apparently, or at least the 
+//number part of it. Decision not defined there either. - Fixed, was trying to match a 
+//number with a string.
